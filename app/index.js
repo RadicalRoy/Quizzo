6965eac32ddff1c7c8cb287d553f739740a9124b
RadicalRoy <rolandquintana1991@gmail.com>
2019-04-23 17:49:44 -0700

    Got webpack working

---
+++ app/index.js
@@ -0,0 +1,21 @@
+let React = require('react');
+let ReactDOM = require('react-dom');
+require('./index.css');
+// state
+// lifecycle events
+// UI
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        Hello World!
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+)
